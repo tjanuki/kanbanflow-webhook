ce3c0fb2ce9912ae93c2c0b541019a84c0e146c1
tjanuki <t.januki@gmail.com>
2024-03-25 16:42:38 -0400

    Modify API method from POST to GET

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/kanbanflow-webhook', [KanbanFlowWebhookController::class, 'handleWebhook'])
+Route::get('/kanbanflow-webhook', [KanbanFlowWebhookController::class, 'handleWebhook'])
 //    ->middleware('auth:sanctum');
 ;
