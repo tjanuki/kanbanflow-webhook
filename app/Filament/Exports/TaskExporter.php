2e2245811694289db8d333c46dc61193a7028db8
tjanuki <t.januki@gmail.com>
2024-04-22 17:27:44 -0400

    Revert summary CSV data

--- app/Filament/Exports/TaskExporter.php
+++ app/Filament/Exports/TaskExporter.php
@@ -6,53 +6,10 @@ use App\Models\Task;
 use Filament\Actions\Exports\ExportColumn;
 use Filament\Actions\Exports\Exporter;
 use Filament\Actions\Exports\Models\Export;
-use Illuminate\Database\Eloquent\Model;
 
 class TaskExporter extends Exporter
 {
     protected static ?string $model = Task::class;
-
-    protected $recordCount;
-    protected $currentRecord = 0;
-    protected $totalSecondsSpent = 0;
-
-    public function __construct(
-        protected Export $export,
-        protected array $columnMap,
-        protected array $options,
-    ) {
-        parent::__construct($export, $columnMap, $options);
-        $this->recordCount = $this->export->total_rows ;
-    }
-
-    public function __invoke(Model $record): array
-    {
-        $this->currentRecord++;
-        $this->record = $record;
-
-        $columns = $this->getCachedColumns();
-
-        $data = [];
-
-        foreach (array_keys($this->columnMap) as $column) {
-            $data[] = $columns[$column]->getFormattedState();
-            if ($column === 'total_seconds_spent') {
-                $this->totalSecondsSpent += $columns[$column]->getFormattedState();
-            }
-        }
-
-        // Append the summary data once all records are processed.
-        if ($this->currentRecord === $this->recordCount) {
-
-            $data = [
-                'date' => 'Grand Total',
-                'name' => '',
-                'total_seconds_spent' => number_format($this->totalSecondsSpent, 1),
-            ];
-        }
-
-        return $data;
-    }
 
     public static function getColumns(): array
     {
