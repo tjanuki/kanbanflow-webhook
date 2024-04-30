94dc71e0a64375c4aee10ab70c51ef77867508fd
tjanuki <t.januki@gmail.com>
2024-04-30 18:50:27 -0400

    Add InitializeEstimation

---
+++ app/Console/Commands/InitializeEstimation.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Estimate;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class InitializeEstimation extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:initialize-estimation {YearMonth? : The year and month to initialize the estimation data for.}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Initialize the estimation data for the specified year and month.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $yearMonth = $this->argument('YearMonth') ?? now()->format('Y-m');
+
+        $this->info("Initializing estimation data for {$yearMonth}...");
+
+        $startDay = Carbon::parse($yearMonth . '-01');
+
+        // Set 6 hours for weekdays and 0 hours for weekends
+        for ($day = $startDay->copy(); $day->lte($startDay->copy()->endOfMonth()); $day->addDay()) {
+            if (Estimate::whereDate('date', $day->toDateString())->exists()) {
+                continue;
+            }
+
+            Estimate::create([
+                'date' => $day,
+                'estimated_seconds' => $day->isWeekday() ? 6 * 3600 : 0,
+            ]);
+        }
+
+        $this->info('Estimation data initialized.');
+    }
+}
