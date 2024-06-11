4a3bf896c00d4afebfc31ca0484d9b58e823dcb3
tjanuki <t.januki@gmail.com>
2024-06-11 16:02:36 -0400

    Add DownloadCsvAction

--- app/Filament/Resources/TaskResource/Actions/DownloadCsvAction.php
+++ app/Filament/Resources/TaskResource/Actions/DownloadCsvAction.php
@@ -1,34 +1,61 @@
 <?php
 
 namespace App\Filament\Resources\TaskResource\Actions;
+use App\Models\Task;
+use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Actions\Action;
+use Illuminate\Support\Carbon;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class DownloadCsvAction extends Action
 {
-
     protected function setUp(): void
     {
         parent::setUp();
         $this->label('Download CSV')
-            ->action(function (): StreamedResponse {
-                return response()->streamDownload(function () {
+            ->form([
+                DatePicker::make('start_date')
+                    ->label('Start Date')
+                    ->default(today()->startOfMonth())
+                    ->required(),
+                DatePicker::make('end_date')
+                    ->label('End Date')
+                    ->default(today()->endOfMonth())
+                    ->required(),
+            ])
+            ->action(function (array $data): StreamedResponse {
+                $startDate = Carbon::parse($data['start_date']);
+                $endDate = Carbon::parse($data['end_date'])->endOfDay();
+
+                return response()->streamDownload(function () use ($startDate, $endDate) {
                     $handle = fopen('php://output', 'w');
-                    fputcsv($handle, ['Header 1', 'Header 2', 'Header 3']);
-                    // Add your data retrieval and CSV generation logic here
+                    fputcsv($handle, ['Date', 'Task Name', 'Spent (Hours)']);
+
+                    $tasks = Task::query()
+                    ->whereBetween('date', [$startDate, $endDate])
+                        ->where('color', 'cyan')
+                        ->orderBy('date', 'desc')
+                        ->get();
+
+                    $totalSpentHours = 0;
+
+                    foreach ($tasks as $task) {
+                        $spentHours = number_format($task->total_seconds_spent / 3600, 1);
+                        $totalSpentHours += (float) $spentHours;
+
+                        fputcsv($handle, [
+                            $task->date,
+                            $task->name,
+                            $spentHours,
+
+                        ]);
+                    }
+
+                    fputcsv($handle, ['Total', '', number_format($totalSpentHours, 1)]);
+
                     fclose($handle);
-                }, 'filename.csv');
+                }, 'tasks_export.csv');
             });
         ;
-    }
-
-    public function handle(): StreamedResponse
-    {
-        return response()->streamDownload(function () {
-            $handle = fopen('php://output', 'w');
-            fputcsv($handle, ['Header 1', 'Header 2', 'Header 3']);
-            // Add your data retrieval and CSV generation logic here
-            fclose($handle);
-        }, 'filename.csv');
     }
 }
